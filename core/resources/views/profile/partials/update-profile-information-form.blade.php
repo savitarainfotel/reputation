8c434e2a31d644e478d1e7d169037647180a9752
Jignesh-sutariya <er.jignesh.sutariya@gmail.com>
2025-07-11 23:12:00 +0530

    Verification Email page completed

--- core/resources/views/profile/partials/update-profile-information-form.blade.php
+++ core/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -6,9 +6,23 @@
         <h6 class="card-title">
             {{ __("Update your account's profile information and email address.") }}
         </h6>
-        <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+        @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            <form id="send-verification" method="post" action="{{ route('verification.send') }}">
             @csrf
-        </form>
+                <div>
+                    <p class="text-sm mt-2">
+                        {{ __('Your email address is unverified.') }}
+                        <x-primary-button type="submit">{{ __('Click here to re-send the verification email.') }}</x-primary-button>
+                    </p>
+
+                    @if (session('status') === 'verification-link-sent')
+                        <p class="mt-2 font-medium text-sm">
+                            {{ __('A new verification link has been sent to your email address.') }}
+                        </p>
+                    @endif
+                </div>
+            </form>
+        @endif
 
         <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
             @csrf
@@ -31,23 +45,7 @@
                 <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
                 <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-                @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                    <div>
-                        <p class="text-sm mt-2 text-gray-800">
-                            {{ __('Your email address is unverified.') }}
-
-                            <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                {{ __('Click here to re-send the verification email.') }}
-                            </button>
-                        </p>
-
-                        @if (session('status') === 'verification-link-sent')
-                            <p class="mt-2 font-medium text-sm text-green-600">
-                                {{ __('A new verification link has been sent to your email address.') }}
-                            </p>
-                        @endif
-                    </div>
-                @endif
+                
             </div>
 
             <div class="mt-2">
