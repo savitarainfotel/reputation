5a5b6a1a3d5a59b8df172eb643731874c4f03cd6
Hemangini-savitara <hemangini@savitarainfotel.com>
2025-07-14 18:38:36 +0530

    login page ui update

--- core/resources/views/layouts/guest.blade.php
+++ core/resources/views/layouts/guest.blade.php
@@ -10,12 +10,13 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     </head>
     <body>
         <div>
-            <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            {{-- <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', 'Laravel') }}</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -49,7 +50,7 @@
                         @endif
                     </div>
                 </div>
-            </nav>
+            </nav> --}}
             <!-- Page Content -->
             <main>
                 <div class="container-fluid">
