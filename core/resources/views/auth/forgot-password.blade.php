7ee8318a30f21772de31d07a8b5077c7c8c101b7
Jignesh-sutariya <er.jignesh.sutariya@gmail.com>
2025-07-14 23:10:34 +0530

    Reset your password page completed

--- core/resources/views/auth/forgot-password.blade.php
+++ core/resources/views/auth/forgot-password.blade.php
@@ -1,31 +1,40 @@
-<x-guest-layout>    
-    <div class="row justify-content-center">
-        <div class="col-12 col-md-4">
-            <div class="card text-white bg-secondary mb-3 mt-4">
-                <div class="card-header">@lang('Log in') </div>
-                <div class="card-body">
-                    <p>
-                        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-                    </p>
-                    <!-- Session Status -->
-                    <x-auth-session-status :status="session('status')" />
+<x-guest-layout>
+    <div id="main-wrapper" class="auth-customizer-none">
+        <div class="position-relative overflow-hidden radial-gradient min-vh-100 w-100">
+            <div class="position-relative z-index-5">
+                <div class="row">
+                    <div class="col-xl-6 col-xxl-6 left-panel">
+                        <x-auth-side :title="__('Welcome Back!')" :sub-title="__('Thousand of professionals Using the #1 AI CRM to monitor, manage and grow online trust.')" />
+                    </div>
+                    <div class="col-xl-6 col-xxl-6">
+                        <div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
+                            <div class="auth-max-width col-sm-8 col-md-6 col-xl-7 px-4">
+                                <h2 class="mb-4 fs-7 fw-bolder">@lang('Reset your password')</h2>
+                                <!-- Session Status -->
+                                <x-auth-session-status :status="session('status')" />
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
 
-                        <!-- Email Address -->
-                        <div>
-                            <x-input-label for="email" :value="__('Email')" />
-                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <!-- Email Address -->
+                                    <div class="mb-3">
+                                        <x-input-label for="email" :value="__('Email Address')" />
+                                        <div class="input-group">
+                                            <span class="input-group-text  border-end-0">
+                                                <img src="{{ asset('assets/images/svg/email.svg') }}" class="icon">
+                                            </span>
+                                            <x-text-input id="email" class="border-start-0 text-dark" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                        </div>
+                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    </div>
+
+                                    <x-primary-button class="w-100 py-8 mt-2 rounded-2">
+                                        {{ __('Send Verification Code') }}
+                                    </x-primary-button>
+                                </form>
+                            </div>
                         </div>
-
-                        <div class="flex items-center justify-end mt-2">
-                            <x-primary-button>
-                                {{ __('Email Password Reset Link') }}
-                            </x-primary-button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
