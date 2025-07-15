452e806001db0e9c66ad10c6afe4088bb42ed401
Hemangini-savitara <hemangini@savitarainfotel.com>
2025-07-15 18:38:23 +0530

    sidebar ui update

--- core/resources/views/layouts/app.blade.php
+++ core/resources/views/layouts/app.blade.php
@@ -59,6 +59,8 @@
         <script src="{{ asset('assets/js/theme/app.init.js') }}"></script>
         <script src="{{ asset('assets/js/theme/theme.js') }}"></script>
         <script src="{{ asset('assets/js/theme/app.min.js') }}"></script>
+        <script src="{{ asset('assets/js/theme/sidebarmenu.js') }}"></script>
+         
 
         <!-- solar icons -->
         <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
--- core/resources/views/layouts/navigation.blade.php
+++ core/resources/views/layouts/navigation.blade.php
@@ -21,21 +21,146 @@
                         <span class="hide-menu">@lang('Dashboard')</span>
                     </a>
                 </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+                        <span class="d-flex">
+                            <i class="ti ti-layout-grid"></i>
+                        </span>
+                        <span class="hide-menu">Answer Feedback</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Review inbox</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Automation</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Review Reply Generator</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Rating Reply Generator</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow " href="javascript:void(0)" aria-expanded="false">
+                        <span class="d-flex">
+                            <i class="ti ti-layout-grid"></i>
+                        </span>
+                        <span class="hide-menu">Property Overview</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Group Overview</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Review Insight</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="javascript:void(0)" class="sidebar-link">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Report</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="../main/index4.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cpu"></i>
+                        </span>
+                        <span class="hide-menu">Survey</span>
+                    </a>
+                </li>
             </ul>
         </nav>
 
-        <div class="fixed-profile p-3 mx-4 mb-2 bg-secondary-subtle rounded mt-3">
-            <div class="hstack gap-3">
+        <div class="fixed-profile   mb-2 mt-3">
+            {{-- <div class="hstack gap-3">
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <button class="border-0 bg-transparent text-primary ms-auto" onclick="event.preventDefault(); this.closest('form').submit();" tabindex="0" type="button" aria-label="logout" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="logout">
+                    <button class="border-0 bg-transparent text-primary ms-auto"
+                        onclick="event.preventDefault(); this.closest('form').submit();" tabindex="0" type="button"
+                        aria-label="logout" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="logout">
                         <i class="ti ti-power fs-7"></i>
                     </button>
                 </form>
-                <a class="border-0 bg-transparent text-primary ms-auto" href="{{ route('profile.edit') }}" tabindex="0" type="button" aria-label="profile" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="profile">
+                <a class="border-0 bg-transparent text-primary ms-auto" href="{{ route('profile.edit') }}"
+                    tabindex="0" type="button" aria-label="profile" data-bs-toggle="tooltip"
+                    data-bs-placement="top" data-bs-title="profile">
                     <i class="icon ti ti-settings fs-7"></i>
                 </a>
-            </div>
+            </div> --}}
+           <nav class="sidebar-nav scroll-sidebar"> 
+            <ul id="sidebarnav">
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('dashboard') }}"aria-expanded="false">
+                        <span>
+                            <i class="ti ti-message-dots"></i>
+                        </span>
+                        <span class="hide-menu">@lang('Seeting')</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="../main/index4.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cpu"></i>
+                        </span>
+                        <span class="hide-menu">Report Bug</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="../main/index4.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cpu"></i>
+                        </span>
+                        <span class="hide-menu">Help</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="../main/index4.html" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cpu"></i>
+                        </span>
+                        <span class="hide-menu">Logout</span>
+                    </a>
+                </li>
+            </ul>
+           </nav>
         </div>
     </div>
 </aside>
