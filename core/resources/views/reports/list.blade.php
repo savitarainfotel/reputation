20a14932ab50add9b6c403c1ada9e1930dd29b31
Hemangini-savitara <hemangini@savitarainfotel.com>
2025-07-24 17:29:55 +0530

    reports page ui update

--- core/resources/views/reports/list.blade.php
+++ core/resources/views/reports/list.blade.php
@@ -1,13 +1,33 @@
 <x-app-layout>
     <div class="row">
-        <h5>Reports</h5>
+        <h5 class="mb-3">Reports</h5>
         <div class="col-lg-4">
             <div class="card border">
                 <img src="{{ asset('assets/images/report1.png') }}" alt="report" srcset="" class="m-2">
                 <div class="card-body">
                     <h5 class="card-title text-dark">Account Report</h5>
                     <p class="card-text">Export statistics for the entire account for detailed comparison of all properties.</p>
-                    <a href="#" class="btn btn-secondary">Generate Account Report <i class="ti ti-arrow-top-right fs-4"></i></a>
+                    <a href="#" class="btn btn-secondary">Generate Account Report <i class="ti ti-arrow-top-right"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card border">
+                <img src="{{ asset('assets/images/report2.png') }}" alt="report" srcset="" class="m-2">
+                <div class="card-body">
+                    <h5 class="card-title text-dark">Account Report</h5>
+                    <p class="card-text">Export statistics for the entire account for detailed comparison of all properties.</p>
+                    <a href="#" class="btn btn-secondary">Generate Account Report <i class="ti ti-arrow-top-right"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card border">
+                <img src="{{ asset('assets/images/report3.png') }}" alt="report" srcset="" class="m-2">
+                <div class="card-body">
+                    <h5 class="card-title text-dark">Account Report</h5>
+                    <p class="card-text">Export statistics for the entire account for detailed comparison of all properties.</p>
+                    <a href="#" class="btn btn-secondary">Generate Account Report <i class="ti ti-arrow-top-right"></i></a>
                 </div>
             </div>
         </div>
