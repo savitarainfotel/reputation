14aa7dbcb6af5ee69be54f04a7582bdf77b27f6f
Jignesh-sutariya <er.jignesh.sutariya@gmail.com>
2025-07-29 16:56:56 +0530

    Transaction added in the scraping listners

--- core/app/Listeners/HandleExpediaReviewsScrape.php
+++ core/app/Listeners/HandleExpediaReviewsScrape.php
@@ -7,6 +7,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Http;
 use App\Events\ExpediaReviewsScrape;
 use App\Models\Review;
+use Illuminate\Support\Facades\DB;
 
 class HandleExpediaReviewsScrape implements ShouldQueue
 {
@@ -24,29 +25,37 @@ class HandleExpediaReviewsScrape implements ShouldQueue
                 $response = $response->json();
 
                 if(!empty($response['reviews'])) {
-                    foreach ($response['reviews'] as $review) {
-                        $newReview                     = new Review();
-                        $newReview->rating_platform_id = $event->ratingSetting->id;
-                        $newReview->title              = $review["text"] ?? null;
-                        $newReview->url                = $review["url"] ?? null;
-                        $newReview->reviewer           = $review["reviewer"] ?? null;
-                        $newReview->reviewer_avatar    = $review["reviewer_avatar"] ?? null;
-                        $newReview->reviewer_id        = $review["reviewer_id"] ?? null;
-                        $newReview->reviewer_url       = $review["reviewer_url"] ?? null;
-                        $newReview->datetime           = $review["datetime"] ?? null;
-                        $newReview->rating             = $review["rating"] ?? null;
-                        $newReview->language           = $review["language"] ?? null;
-                        $newReview->likes              = $review["likes"] ?? null;
-                        $newReview->reply              = $review["reply"] ?? null;
-                        $newReview->sentiment          = $review["sentiment"] ?? null;
-                        $newReview->stay_text          = $review["stay_text"] ?? null;
-                        $newReview->created_by         = $event->property->created_by;
-                        $newReview->updated_by         = $event->property->updated_by;
-                        $newReview->save();
-                    }
+                    DB::beginTransaction();
 
-                    $event->property->reviews += count($response['reviews']);
-                    $event->property->save();
+                    try {
+                        foreach ($response['reviews'] as $review) {
+                            $newReview                     = new Review();
+                            $newReview->rating_platform_id = $event->ratingSetting->id;
+                            $newReview->title              = $review["text"] ?? null;
+                            $newReview->url                = $review["url"] ?? null;
+                            $newReview->reviewer           = $review["reviewer"] ?? null;
+                            $newReview->reviewer_avatar    = $review["reviewer_avatar"] ?? null;
+                            $newReview->reviewer_id        = $review["reviewer_id"] ?? null;
+                            $newReview->reviewer_url       = $review["reviewer_url"] ?? null;
+                            $newReview->datetime           = $review["datetime"] ?? null;
+                            $newReview->rating             = $review["rating"] ?? null;
+                            $newReview->language           = $review["language"] ?? null;
+                            $newReview->likes              = $review["likes"] ?? null;
+                            $newReview->reply              = $review["reply"] ?? null;
+                            $newReview->sentiment          = $review["sentiment"] ?? null;
+                            $newReview->stay_text          = $review["stay_text"] ?? null;
+                            $newReview->created_by         = $event->property->created_by;
+                            $newReview->updated_by         = $event->property->updated_by;
+                            $newReview->save();
+                        }
+
+                        $event->property->reviews += count($response['reviews']);
+                        $event->property->save();
+
+                        DB::commit();
+                    } catch (\Throwable $th) {
+                        DB::rollBack();
+                    }
                 }
             }
         }
--- core/app/Listeners/HandleGoogleReviewsScrape.php
+++ core/app/Listeners/HandleGoogleReviewsScrape.php
@@ -7,6 +7,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Http;
 use App\Events\GoogleReviewsScrape;
 use App\Models\Review;
+use Illuminate\Support\Facades\DB;
 
 class HandleGoogleReviewsScrape implements ShouldQueue
 {
@@ -23,27 +24,35 @@ class HandleGoogleReviewsScrape implements ShouldQueue
             $response = $response->json();
 
             if(!empty($response['reviews'])) {
-                foreach ($response['reviews'] as $review) {
-                    $newReview                     = new Review();
-                    $newReview->rating_platform_id = $event->ratingSetting->id;
-                    $newReview->title              = $review["text"] ?? null;
-                    $newReview->url                = $review["url"] ?? null;
-                    $newReview->reviewer           = $review["reviewer"] ?? null;
-                    $newReview->reviewer_avatar    = $review["reviewer_avatar"] ?? null;
-                    $newReview->reviewer_id        = $review["reviewer_id"] ?? null;
-                    $newReview->reviewer_url       = $review["reviewer_url"] ?? null;
-                    $newReview->datetime           = $review["datetime"] ?? null;
-                    $newReview->rating             = $review["rating"] ?? null;
-                    $newReview->language           = $review["language"] ?? null;
-                    $newReview->likes              = $review["likes"] ?? null;
-                    $newReview->reply              = $review["reply"] ?? null;
-                    $newReview->created_by         = $event->property->created_by;
-                    $newReview->updated_by         = $event->property->updated_by;
-                    $newReview->save();
-                }
+                DB::beginTransaction();
 
-                $event->property->reviews = count($response['reviews']);
-                $event->property->save();
+                try {
+                    foreach ($response['reviews'] as $review) {
+                        $newReview                     = new Review();
+                        $newReview->rating_platform_id = $event->ratingSetting->id;
+                        $newReview->title              = $review["text"] ?? null;
+                        $newReview->url                = $review["url"] ?? null;
+                        $newReview->reviewer           = $review["reviewer"] ?? null;
+                        $newReview->reviewer_avatar    = $review["reviewer_avatar"] ?? null;
+                        $newReview->reviewer_id        = $review["reviewer_id"] ?? null;
+                        $newReview->reviewer_url       = $review["reviewer_url"] ?? null;
+                        $newReview->datetime           = $review["datetime"] ?? null;
+                        $newReview->rating             = $review["rating"] ?? null;
+                        $newReview->language           = $review["language"] ?? null;
+                        $newReview->likes              = $review["likes"] ?? null;
+                        $newReview->reply              = $review["reply"] ?? null;
+                        $newReview->created_by         = $event->property->created_by;
+                        $newReview->updated_by         = $event->property->updated_by;
+                        $newReview->save();
+                    }
+
+                    $event->property->reviews = count($response['reviews']);
+                    $event->property->save();
+
+                    DB::commit();
+                } catch (\Throwable $th) {
+                    DB::rollBack();
+                }
             }
         }
     }
